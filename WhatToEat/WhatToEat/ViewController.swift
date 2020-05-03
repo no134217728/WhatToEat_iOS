902970327fcfa60ab9a2d36759e9f86c6940eac4
Cart00nHero8 <Cart00nHero8@gmail.com>
2020-05-03 13:42:59 +0800

    Initialized project

---
+++ WhatToEat/WhatToEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatToEat
+//
+//  Created by YuCheng on 2020/5/3.
+//  Copyright © 2020 YuCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
