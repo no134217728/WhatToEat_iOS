aca507d459c3172d8c485e0e8e3797ceff20bee2
Cart00nHero8 <Cart00nHero8@gmail.com>
2020-05-09 14:38:56 +0800

    Add Features and Templates

---
+++ WhatToEat/WhatToEat/SourceCode/Redux/Actions/RouteActions.swift
@@ -0,0 +1,13 @@
+//
+//  RouteActions.swift
+//  WhatToEat
+//
+//  Created by YuCheng on 2020/5/9.
+//  Copyright © 2020 YuCheng. All rights reserved.
+//
+
+import UIKit
+
+class RouteActions: NSObject {
+
+}
---
+++ WhatToEat/WhatToEat/SourceCode/Templates/UIBaseViewController.swift
@@ -0,0 +1,113 @@
+//
+//  UIBaseViewController.swift
+//  EventX
+//
+//  Created by YuCheng on 2018/11/11.
+//  Copyright © 2018 EventXtra. All rights reserved.
+//
+
+import UIKit
+
+class UIBaseViewController: UIViewController {
+
+    @IBOutlet public weak var baseNavigationBar: UIView?
+    @IBOutlet public weak var navTitleLabel: UILabel?
+    @IBOutlet public weak var navLeftButton: UIButton?
+    @IBOutlet private weak var containerView: UIView?
+    @IBOutlet private weak var navBarHeightConstraint: NSLayoutConstraint?
+
+    var childVC: UIViewController? = nil
+
+    var navButtonClickActionSelector: Selector?
+    var buttonClickActionSelector: Selector?
+    var defaultNavHeight: CGFloat?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        if (containerViewController?.title?.count)! > 0 {
+//            self.navTitleLabel?.text = containerViewController!.title
+//        }
+        defaultNavHeight = navBarHeightConstraint?.constant
+        if childVC != nil {
+            for vc in self.children {
+                vc.willMove(toParent: nil)
+                vc.view.removeFromSuperview()
+                vc.removeFromParent()
+            }
+            self.addChild(childVC!)
+            childVC?.view.frame = containerView!.bounds
+            containerView?.addSubview(childVC!.view)
+            childVC?.didMove(toParent: self)
+        }
+    }
+
+    // MARK: - public
+    public final func baseContainerView() -> UIView {
+        return containerView!
+    }
+
+    public final func basePushToViewController(_ viewController: UIViewController, Animated anmated: Bool) {
+        let newBaseVC: UIBaseViewController = self.storyboard?.instantiateViewController(withIdentifier: self.restorationIdentifier!) as? UIBaseViewController ?? UIBaseViewController()
+        newBaseVC.childVC = viewController
+        self.navigationController?.pushViewController(newBaseVC, animated: anmated)
+    }
+
+    public final func basePresentViewController(_ viewController: UIViewController, Animated anmated: Bool) {
+        let newBaseVC: UIBaseViewController = self.storyboard?.instantiateViewController(withIdentifier: self.restorationIdentifier!) as? UIBaseViewController ?? UIBaseViewController()
+        newBaseVC.childVC = viewController
+        self.present(newBaseVC, animated: anmated, completion: nil)
+    }
+
+    public final func hideBaseNavigationBar() {
+        navBarHeightConstraint?.constant = 0
+        baseNavigationBar?.isHidden = true
+    }
+
+    public final func displayBaseNavigationBar() {
+        navBarHeightConstraint?.constant = defaultNavHeight!
+        baseNavigationBar?.isHidden = false
+    }
+
+    // MARK: - public
+    public final func addTarget(_ aTarget: Any, navButtonClickAction selector: Selector) {
+        navButtonClickActionSelector = selector
+        if childVC == nil {
+            childVC = aTarget as? UIViewController
+        }
+    }
+
+    public final func addTarget(_ aTarget: Any, buttonClickAction selector: Selector) {
+        buttonClickActionSelector = selector
+        if childVC == nil {
+            childVC = aTarget as? UIViewController
+        }
+    }
+
+    // MARK: - action
+    @IBAction func navButtonClickAction(_ sender: UIButton) {
+        if navButtonClickActionSelector != nil && (childVC?.responds(to: navButtonClickActionSelector))! {
+            // clang diagnostic push
+            // clang diagnostic ignored "-Warc-performSelector-leaks"
+            childVC?.performSelector(inBackground: navButtonClickActionSelector!, with: sender)
+            // clang diagnostic pop
+        } else {
+            if (self.presentingViewController != nil) {
+                self.navigationController?.dismiss(animated: true, completion: nil)
+            } else {
+                self.navigationController?.popViewController(animated: true)
+
+            }
+            
+        }
+    }
+
+    @IBAction func buttonClickAction(_ sender: UIButton) {
+        if buttonClickActionSelector != nil && (childVC?.responds(to: buttonClickActionSelector))! {
+            // clang diagnostic push
+            // clang diagnostic ignored "-Warc-performSelector-leaks"
+            childVC?.performSelector(inBackground: buttonClickActionSelector!, with: sender)
+            // clang diagnostic pop
+        }
+    }
+}
